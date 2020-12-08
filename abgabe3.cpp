12d073c9916b9ffd80aa24cf8586307b003ebe58
Paul Wirkus <paul.wirkus@rwth-aachen.de>
2020-12-08 11:35:51 +0100

    add A31*

--- abgabe3.cpp
+++ abgabe3.cpp
@@ -13,14 +13,15 @@ int min_pos(const double *feld, int n);
 void sortiere(double *feld, int n);
 void umdrehen(double *feld, int n);
 double* glue( const double *feld1, int n1, const double *feld2, int n2);
+int binSuche_pos( double x, const double *sfeld, int n);
 
 int main() {
     // insert code here...
     double feld[] = {47, 1.1, 0, 8, 1.5};
-    double feld1[] = {66, 44, 2, 3.1415};
-    double *glued = glue(feld, 5, feld1, 4);
-    Ausgabe(glued, 9);
-    delete [] glued;
+    sortiere(feld, 5);
+    int f = binSuche_pos(47, feld, 5);
+    std::cout << f << std::endl;
+
     return 0;
 }
 
@@ -88,4 +89,27 @@ double* glue( const double *feld1, int n1, const double *feld2, int n2)
         feld[n1+i] = feld2[i];
     }
     return feld;
+}
+
+int binSuche_pos( double x, const double *sfeld, int n)
+{
+    if (n<=0)
+        return -1;
+    int m = n/2;
+    double a = sfeld[m];
+    if (x < a)
+        return binSuche_pos(x, sfeld, m);
+    else if ( x == a )
+        return m;
+    else if (x > a)
+    {
+        if ( (n % 2) == 0 )
+        {
+            return binSuche_pos(x, sfeld+(m+1), m-1)+(m+1);
+        } else
+        {
+            return binSuche_pos(x, sfeld+(m+1), m)+(m+1);
+        }
+    }
+    return -1;
 }
